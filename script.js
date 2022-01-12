4fc4d9d858666fe135c4e91d443695e3c79ee23c
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-01-12 12:00:06 +0300

    update 3

---
+++ script.js
@@ -0,0 +1,12 @@
+// function openMenu()
+// {
+//     document.getElementById('mobail-nav').style.display="block";
+// }
+// function closeMenu()
+// {
+//     document.getElementById('mobail-nav').style.display="none";
+// }
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
